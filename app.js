58e610e29c83f527564a35a25f9ee00fb12ccaf8
Guanyu <guanyuchen630602@gmail.com>
2025-02-10 14:06:48 +0800

    remove cors

--- app.js
+++ app.js
@@ -6,35 +6,35 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 
 // cors
-const corsOptions = {
-	origin: '*',
-	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-	credentials: true,
-	optionsSuccessStatus: 200,
-	maxAge: 3600,
-}
+// const corsOptions = {
+// 	origin: '*',
+// 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+// 	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+// 	credentials: true,
+// 	optionsSuccessStatus: 200,
+// 	maxAge: 3600,
+// }
 
-app.use((req, res, next) => {
-	const cors = require('cors')(corsOptions)
+// app.use((req, res, next) => {
+// 	const cors = require('cors')(corsOptions)
 
-	cors(req, res, err => {
-		if (err) {
-			console.error('CORS Error:', {
-				origin: req.get('origin'),
-				method: req.method,
-				errorMessage: err.message,
-			})
+// 	cors(req, res, err => {
+// 		if (err) {
+// 			console.error('CORS Error:', {
+// 				origin: req.get('origin'),
+// 				method: req.method,
+// 				errorMessage: err.message,
+// 			})
 
-			return res.status(403).json({
-				status: 'error',
-				message: 'Cross-Origin Request Blocked',
-				details: 'The request origin is not allowed by CORS policy',
-			})
-		}
-		next()
-	})
-})
+// 			return res.status(403).json({
+// 				status: 'error',
+// 				message: 'Cross-Origin Request Blocked',
+// 				details: 'The request origin is not allowed by CORS policy',
+// 			})
+// 		}
+// 		next()
+// 	})
+// })
 
 app.use(cookieParser())
 app.use(express.json())
